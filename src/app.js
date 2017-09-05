9d5bea00e1dbe1f279bdbe7c0e0062c83076de29
katrina <klee@thoughtworks.com>
2017-09-05 16:22:27 +1000

    Carol M, Katrina L- Added header, styled header and image with styles object

--- src/app.js
+++ src/app.js
@@ -6,14 +6,34 @@ import {
 } from 'react-native';
 
 const App = () => (
-  <View style={styles.app}>
-      <Image source={require('./resources/movieBoardImg.jpg')} />
-      <Text>What movie should we watch tonight?</Text>
+  <View style={styles.home}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>What movie should we watch tonight?</Text>
+      </View>
+      <View style={styles.homeContent}>
+        <Image source={require('./resources/movieBoardImg.jpg')} />
+      </View>
   </View>
 )
 
 const styles = {
-  app: {
+  home: {
+    flex:1
+  },
+  headerText: {
+    textAlign:'center'
+  },
+  header: {
+    backgroundColor: '#F8F8F8',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+  },
+  homeContent: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
