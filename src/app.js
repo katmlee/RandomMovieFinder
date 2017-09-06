1c71ef089f196af42029f374c8683749096526ae
katrina <klee@thoughtworks.com>
2017-09-06 15:11:58 +1000

    Carol M, Katrina L- Extracted Home component out of App

--- src/app.js
+++ src/app.js
@@ -1,110 +1,18 @@
 import React, { Component } from 'react';
-import fakeMoviesApi from './fakeMoviesApi';
-import {
-  Text,
-  View,
-  Image,
-  TextInput,
-  ScrollView,
-} from 'react-native';
-import Header from './Header';
+import { View } from 'react-native';
+import Home from './components/Home';
+import Header from './components/Header';
 
-const state = {
-  inputValue: '',
-  movies: [],
-}
-
-
-class App extends Component {
-  constructor() {
-    super()
-    this.state = state
-  }
-
-  onChangeText = (inputValue) => {
-    this.setState({inputValue})
-    this.searchMovies(inputValue)
-  }
-
-  searchMovies = (keyword) => {
-    this.setState({movies: fakeMoviesApi(keyword)});
-  }
-
-  renderSearchWord = () => {
-    if (this.state.inputValue !== '') {
-      return (
-        <Text style={styles.currentSearch}>
-          {`Searches for '${this.state.inputValue}'`}
-        </Text>
-    )}
-  }
-
-  renderList = () => {
-    if (this.state.movies && this.state.movies.length > 0) {
-      return this.state.movies.map(movie =>
-        <Text key={movie.id}>{movie.title}</Text>
-      );
-    }
-  }
-
-  render() {
-    console.log(this.state.inputValue)
-    return (
-      <View style={styles.home}>
-          <Header> Random Movie Finder </Header>
-          <View style={styles.homeContent}>
-            <Text>
-              What movie should we watch tonight?
-            </Text>
-            <Image
-              style={styles.image}
-              source={require('./resources/movieBoardImg.jpg')}
-            />
-            <TextInput
-              style={styles.searchBar}
-              placeholder='Search anything...'
-              onChangeText={this.onChangeText}
-            />
-            {this.renderSearchWord()}
-            <ScrollView style={styles.searchResults}>
-              {this.renderList()}
-            </ScrollView>
-          </View>
-      </View>
-    )
-  }
-}
+const App = () => (
+  <View style={styles.app}>
+    <Header>Random Movie Finder</Header>
+    <Home />
+  </View>
+);
 
 const styles = {
-  home: {
+  app: {
     flex:1
-  },
-  image: {
-    width: '30%',
-    height: '30%',
-    resizeMode: 'contain',
-  },
-  homeContent: {
-    flex: 1,
-    paddingTop: 15,
-    alignItems: 'center'
-  },
-  searchBar: {
-    height: 40,
-    width: '70%',
-    padding: 10,
-    borderRadius: 2,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  searchResults:{
-    width:'100%',
-    paddingLeft:'15%',
-    paddingRight:'15%',
-  },
-  currentSearch: {
-    paddingTop: 16,
-    fontWeight: 'bold',
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import fakeMoviesApi from '../fakeMoviesApi';
+import {
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+
+const state = {
+  inputValue: '',
+  movies: [],
+}
+
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = state
+  }
+
+  onChangeText = (inputValue) => {
+    this.setState({inputValue})
+    this.searchMovies(inputValue)
+  }
+
+  searchMovies = (keyword) => {
+    this.setState({movies: fakeMoviesApi(keyword)});
+  }
+
+  renderSearchWord = () => {
+    if (this.state.inputValue !== '') {
+      return (
+        <Text style={styles.currentSearch}>
+          {`Searches for '${this.state.inputValue}'`}
+        </Text>
+    )}
+  }
+
+  renderList = () => {
+    if (this.state.movies && this.state.movies.length > 0) {
+      return this.state.movies.map(movie =>
+        <Text key={movie.id}>{movie.title}</Text>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.homeContent}>
+        <Text>
+          What movie should we watch tonight?
+        </Text>
+        <Image
+          style={styles.image}
+          source={require('../resources/movieBoardImg.jpg')}
+        />
+        <TextInput
+          style={styles.searchBar}
+          placeholder='Search anything...'
+          onChangeText={this.onChangeText}
+        />
+        {this.renderSearchWord()}
+        <ScrollView style={styles.searchResults}>
+          {this.renderList()}
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  image: {
+    width: '30%',
+    height: '30%',
+    resizeMode: 'contain',
+  },
+  homeContent: {
+    flex: 1,
+    paddingTop: 15,
+    alignItems: 'center'
+  },
+  searchBar: {
+    height: 40,
+    width: '70%',
+    padding: 10,
+    borderRadius: 2,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  searchResults:{
+    width:'100%',
+    paddingLeft:'15%',
+    paddingRight:'15%',
+  },
+  currentSearch: {
+    paddingTop: 16,
+    fontWeight: 'bold',
+  }
+}
+
+export default Home;
