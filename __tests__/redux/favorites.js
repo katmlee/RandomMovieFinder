798fe3ec8c3606ec11c5c15adf77465596bfcdf0
Carol Mak <cmak@thoughtworks.com>
2017-09-25 12:18:11 +1000

    Testing simple favorites actions/reducers

---
+++ __tests__/redux/favorites.js
@@ -0,0 +1,37 @@
+import reducer, { addFavorite, removeFavorite, ADD_FAVORITE, REMOVE_FAVORITE } from '../../src/redux/favorites';
+
+describe('favorites', () => {
+  it('should addFavorite', () => {
+    const movie = {};
+    expect(addFavorite(movie)).toEqual({
+      type: ADD_FAVORITE,
+      movie,
+    });
+  });
+
+  it('should removeFavorite', () => {
+    const movie = {};
+    expect(removeFavorite(movie)).toEqual({
+      type: REMOVE_FAVORITE,
+      movie,
+    });
+  });
+
+  it('should append movie', () => {
+    const movie = {};
+    const action = {
+      type: ADD_FAVORITE,
+      movie,
+    };
+    expect(reducer([], action)).toEqual([movie]);
+  });
+
+  it('should remove movie', () => {
+    const movie = {};
+    const action = {
+      type: REMOVE_FAVORITE,
+      movie,
+    };
+    expect(reducer([movie], action)).toEqual([]);
+  });
+});
